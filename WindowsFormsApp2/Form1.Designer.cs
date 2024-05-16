be4e615b6e5f7b03606b4240067082ee9527a2e6
def-lab <vinside96@bk.ru>
2024-05-16 18:40:58 +0300

    first commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Bab = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Bab)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Bab
+            // 
+            this.Bab.Location = new System.Drawing.Point(374, 227);
+            this.Bab.Name = "Bab";
+            this.Bab.Size = new System.Drawing.Size(100, 50);
+            this.Bab.TabIndex = 0;
+            this.Bab.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Bab);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Bab)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Bab;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.MaximumSize = new Size(9000, 1100);
+            this.MinimumSize = new Size(800, 900);
+            
+            this.DoubleBuffered = true;
+            
+            Bab.Image = Image.FromFile(@"C:\Users\Aogiri\source\repos\WindowsFormsApp2\WindowsFormsApp2\butterfly\butterfly.png");
+            Bab.SizeMode = PictureBoxSizeMode.StretchImage;
+            Bab.BorderStyle = BorderStyle.None;
+            Bab.BackColor = Color.Transparent;
+            Bab.MouseEnter += new EventHandler(this.Bab_MouseEnter);
+            Bab.MouseMove += new MouseEventHandler(Bab_MouseMove);
+            //this.MouseMove += new MouseEventHandler(Form1_MouseMove);
+            Bab.MouseLeave += new EventHandler(Bab_MouseLeave);
+        }
+        private bool isMove = false;
+
+
+        private void Bab_MouseEnter(object sender, EventArgs e)
+        {
+            isMove = true;
+            Control control = (Control)sender;
+            Point mousePos = control.PointToClient(MousePosition);
+
+
+        }
+        private const double SlowDownFactor = 0.1; // Коэффициент замедления
+
+        private void Bab_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMove)
+            {
+                Control control = (Control)sender;
+
+
+                int dx = MousePosition.X - control.Left;
+                int dy = MousePosition.Y - control.Top;
+
+
+                double slowDownFactor = 0.05;
+
+
+                int newX = control.Left + (int)(dx * slowDownFactor);
+                int newY = control.Top + (int)(dy * slowDownFactor);
+
+
+                control.Left = newX;
+                control.Top = newY;
+            }
+        }
+
+
+
+        private void Bab_MouseLeave(object sender, EventArgs e)
+        {
+            isMove = false;
+        }
+    }
+}
